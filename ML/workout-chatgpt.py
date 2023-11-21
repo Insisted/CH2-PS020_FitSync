2c1f3040a9674ee8a7328bcd6c2693a0635e67f8
Steven <steven.tribethran@example.com>
2023-11-21 15:27:08 +0700

    UPD: Create template message

--- ML/workout-chatgpt.py
+++ ML/workout-chatgpt.py
@@ -6,7 +6,19 @@ import openai
 
 openai.api_key = os.environ.get('ChatGPT')
 MODEL = 'gpt-3.5-turbo'
-messages = [ {'role': 'system', 'content': 'You are a intelligent workout/fitness coach.'} ]
+messages = [ {
+	'role': 'system',
+	'content': '''
+You are an intelligent workout/fitness coach. I have a task that would need me to classify a workout as "Beginner|Intermediate|Expert" level, help me with that. Answer me with a JSON response in this structure
+```
+{
+    "name": "{What I asked you}",
+    "desc": "{The description of the workout}",
+    "level": "{The level of the workout}"
+}
+```
+'''
+} ]
 
 
 def start(df): # Biarin dulu, test APInya jalan ga pake input manual
