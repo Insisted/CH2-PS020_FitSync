ae111d10f807efeb4e0123cbdcacc9862a4a80f8
Steven <steven.tribethran@gmail.com>
2023-11-22 02:27:07 +0700

    UPD: Desc and Level Addition

--- ML/workout-chatgpt.py
+++ ML/workout-chatgpt.py
@@ -1,36 +1,40 @@
+import asyncio
 import os
 
 import pandas as pd
-import openai
+from api_key import OPENAI_API_KEY
+from openai import AsyncOpenAI
 
-
-openai.api_key = os.environ.get('ChatGPT')
 MODEL = 'gpt-3.5-turbo'
-messages = [ {
+messages = [{
 	'role': 'system',
 	'content': '''
-You are an intelligent workout/fitness coach. I have a task that would need me to classify a workout as "Beginner|Intermediate|Expert" level, help me with that. Answer me with a JSON response in this structure
+You are an intelligent workout/fitness coach. I have a task that would need me to classify a workout as "Beginner|Intermediate|Expert" level with respect of the data as "Male|Female", help me with that. Answer me with a JSON response in this structure
 ```
 {
     "name": "{What I asked you}",
+    "gender": "{Gender based on the input}"
     "desc": "{The description of the workout}",
     "level": "{The level of the workout}"
 }
 ```
+No explaining no nothing.
 '''
-} ]
+}]
+
+client = AsyncOpenAI(api_key=OPENAI_API_KEY)
 
 
-def start(df): # Biarin dulu, test APInya jalan ga pake input manual
+async def main(df):
 	message = input('User : ') 
+	
+	if message:
+		messages.append({'role': 'user', 'content': message}) 
 
-	if message: 
-		messages.append( 
-			{'role': 'user', 'content': message}, 
-		) 
-		chat = openai.ChatCompletion.create( 
-			model=MODEL, messages=messages 
-		) 
+		chat = await client.chat.completions.create(
+			messages=messages,
+			model=MODEL,
+		)
 
 	reply = chat.choices[0].message.content 
 
@@ -40,6 +44,8 @@ def start(df): # Biarin dulu, test APInya jalan ga pake input manual
 
 
 if __name__ == '__main__':
-	df = pd.read_excel('./data/Gym-Visual-EXERCISES-list.xlsx', sheet_name='Animated GIFs')
+	df = pd.read_excel('./ML/data/Gym-Visual-EXERCISES-list.xlsx', sheet_name='Animated GIFs')
 
-	start(df)
+	asyncio.run(main(df))
+
+	print(messages)
