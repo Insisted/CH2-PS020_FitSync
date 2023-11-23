f848d53f5e8670ed3fe11ad50786327c9bc7ee22
Steven <steven.tribethran@example.com>
2023-11-23 10:59:51 +0700

    UPD: Fix Dataset Structure

--- ML/data/gymvisual-helper.py
+++ ML/data/gymvisual-helper.py
@@ -16,23 +16,41 @@ def clean(df, gym):
         c = re.sub('\"title\": \"{(.+)}', '\"title\": \"\\1', b)
         res = eval(re.sub(r'Q:.+', '', c))
 
-        temp.append({
-            'title': dataf[0],
-            'type': dataf[1],
-            'body_part': dataf[2],
-            'gender': dataf[3],
-            'desc': res['desc'],
-            'level': res['level'],
-            'time': res['time']
-        })
+        try:
+            temp.append({
+                'title': dataf[0],
+                'type': dataf[1],
+                'body_part': dataf[2],
+                'gender': dataf[3],
+                'desc': res['desc'],
+                'level': res['level'],
+                'duration': parse_time(res['time'])
+            })
+        except: print(res)
 
     return temp
+
+
+def parse_time(time):
+    parsed = re.findall(r'([0-9-]+)\s*(\w+)', time)
+    res = dict()
+
+    for i in parsed:
+        unit = i[1][:3]
+
+        if re.match(r'\d+\s*sets*\s*of\s*\d+\s*(min|sec)\w+', time) and unit in ['sec', 'min']:
+            res['desc'] = time
+            res[unit] = str(int(i[0]) * int(res['set']))
+        else:
+            res[unit] = i[0]
+
+    return res
 
 
 def get_vis(clean, vis):
     clean = pd.DataFrame(clean)
     vis = pd.DataFrame(vis)
-    merged = pd.merge(clean, vis, on='title').drop_duplicates(subset=['title'])
+    merged = pd.merge(clean, vis, on='title')
 
     print(len(clean), len(vis), len(merged))
 
