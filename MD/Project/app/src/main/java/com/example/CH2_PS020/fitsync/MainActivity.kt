3bbedcd261ef4008dc5917b2721014f76b6e167c
Vincent-2125250004 <vincent.cent@mhs.mdp.ac.id>
2023-11-27 01:02:32 +0700

    -ui: Adding Material Theme, Update Bottom Navigation View
    Styling with Material Design theme and Update Bottom Navigation View Appearance

--- MD/Project/app/src/main/java/com/example/CH2_PS020/fitsync/MainActivity.kt
+++ MD/Project/app/src/main/java/com/example/CH2_PS020/fitsync/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.CH2_PS020.fitsync
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.view.MenuItem
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.CH2_PS020.fitsync.databinding.ActivityMainBinding
 import com.example.CH2_PS020.fitsync.ui.account.AccountFragment
 import com.example.CH2_PS020.fitsync.ui.home.HomeFragment
 import com.example.CH2_PS020.fitsync.ui.tracker.TrackerFragment
 import com.example.CH2_PS020.fitsync.ui.workout.WorkoutFragment
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,20 +25,28 @@ class MainActivity : AppCompatActivity() {
         val trackerFragment = TrackerFragment()
         val accountFragment = AccountFragment()
         setFragment(homeFragment)
+
+        val shapeDrawable : MaterialShapeDrawable= binding.bottomNavigationView.background as MaterialShapeDrawable
+        shapeDrawable.shapeAppearanceModel = shapeDrawable.shapeAppearanceModel
+            .toBuilder()
+            .setTopLeftCorner(CornerFamily.ROUNDED, 70F)
+            .setTopRightCorner(CornerFamily.ROUNDED, 70F)
+            .build()
+
         binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.menu_home    ->setFragment(homeFragment)
+            when (it.itemId) {
+                R.id.menu_home -> setFragment(homeFragment)
                 R.id.menu_workout -> setFragment(workoutFragment)
                 R.id.menu_tracker -> setFragment(trackerFragment)
                 R.id.menu_account -> setFragment(accountFragment)
                 else -> false
             }
         }
+
+
     }
 
-
-
-    private fun setFragment(fragment:Fragment):Boolean {
+    private fun setFragment(fragment: Fragment): Boolean {
         supportFragmentManager
             .beginTransaction()
             .replace(binding.frameLayout.id, fragment)
